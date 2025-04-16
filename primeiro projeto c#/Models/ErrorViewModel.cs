9171fb30d30fa49a5bfb8d15ff8ae945f059f3e9
João Pedro <joaopedro.b.c@hotmail.com>
2025-04-16 00:25:30 -0300

    Adicionar arquivos de projeto.

---
+++ primeiro projeto c#/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace primeiro_projeto_c_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

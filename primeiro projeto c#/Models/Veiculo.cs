2d3dc6446054cb469aee27c6dc33c770ee5ac7c4
João Pedro <joaopedro.b.c@hotmail.com>
2025-04-18 09:30:26 -0300

    Configuração do Banco de Dados e da Tabela Veículo 2

---
+++ primeiro projeto c#/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace primeiro_projeto_c_.Models
+{
+
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public int AnoModelo { get; set; }
+    }
+}
